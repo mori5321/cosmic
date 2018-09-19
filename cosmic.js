3e6c54db292d13133eb290224e7d00901fa1d32b
西森大介 <nishimori.daisuke@di-v.co.jp>
2018-09-19 21:18:49 +0900

    fix bug

--- cosmic.js
+++ cosmic.js
@@ -4,46 +4,56 @@ class CosmicSpace {
     this.target = target
     this.maxLength = parseInt(target.getAttribute('data-maxlength'))
     this.gap = parseInt(target.getAttribute('data-gap'))
+    this.spaceLength = this.calculateSpaceLength()
     this.backSpaceKeyCode =  8
     this.deleteKeyCode = 46
-    this.setCosmicSpaceEvent()
+    this.setMaxLength()
+    this.listen()
   }
 
 
   listen() {
+    var _this = this
     this.target.addEventListener("keydown", function(e) {
-      var trimed_sentence = this.trimmed_string(e.target.value)
-      if (trimed_sentence.length >= this.maxlength) {
-        return
-      }
+      var sentence = e.target.value
+      var trimmed_sentence = _this.trimmed_string(sentence)
 
-      if (trimed_sentence.length % gap === 0 && trimed_sentence.length > 0) {
+
+      if (trimmed_sentence.length % _this.gap === 0 && trimmed_sentence.length > 0) {
         sentence = sentence + " "
       }
 
-      if (e.keyCode === this.backSpaceKeyCode || e.keyCode === this.deleteKeyCode) {
-        sentence = removeSpaceFromTail(e.target.value)
+      if (e.keyCode === _this.backSpaceKeyCode || e.keyCode === _this.deleteKeyCode) {
+        sentence = _this.removeSpaceFromTail(sentence)
       }
+
+      console.log(sentence)
 
       e.target.value = sentence
     })
   }
 
-  trimmed_string(string) {
-    string.replace(/\s+/, "")
+  setMaxLength() {
+    var maxLength = this.maxLengthWithoutSpace()
+    this.target.setAttribute('maxlength', maxLength)
   }
 
-  spaceLength(maxLength, gap) {
-    Math.floor( (maxLength - 1) / gap )
+  trimmed_string(string) {
+    return string.replace(/\s+/g, "")
+  }
+
+  calculateSpaceLength() {
+    return Math.floor( (this.maxLength - 1) / this.gap )
   }
 
   maxLengthWithoutSpace(maxLength, spaceLength) {
-    maxLength + spaceLength
+    return this.maxLength + this.spaceLength
   }
 
   removeSpaceFromTail(text) {
-    if ( text.slice(-1).match(/\s+/) ) {
-       text.slice(0, -1)
+    if (text.slice(-1).match(/\s+/g) ) {
+      return text.slice(0, -1)
     }
+    return text
   }
 }
